0cefcafd01c3a95da0ad5633c1c6f1571c5d118b
llNetoll <joaomoraisneto2000@gmail.com>
2025-05-27 16:07:06 -0300

    Add files via upload

---
+++ Trabalho 1/t1q8.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+
+    char matriz[3][3], jogada[3];
+
+    printf("jogador 1 informe a sua jogada: "); scanf(" %[^\n]",jogada[3]);
+    printf("%s",jogada);
+
+
+
+}
